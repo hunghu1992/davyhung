7aed8561f7708b7cd6a8c688601d2aff5254b4ef
davy <534900793@qq.com>
2024-02-06 00:14:43 +0800

    test2

--- pages/uploadpage/uploadpage.js
+++ pages/uploadpage/uploadpage.js
@@ -1,61 +1,52 @@
 Page({
-  data: {
-    imageUrl: '',
-    showImage: false,
-    uploadResult: '',
-  },
-  chooseImage: function() {
-    wx.chooseImage({
-      count: 9, // 默认9
-      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
-      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
-      success: (res) => {
-        const tempFilePaths = res.tempFilePaths
-        this.uploadFile(tempFilePaths[0])
-      },
-      fail: (err) => {
-        this.setData({ uploadResult: '选择图片失败：' + err.errMsg });
+  async onLoad() {
+    wx.cloud.init({
+      env: 'prod-0gi5h53v7bb1a7c8',
+    })
+    const that = this
+    await wx.chooseImage({
+      count: 1,
+      async success(res){
+        console.log(res.tempFilePaths)
+        const result = await that.uploadFile(res.tempFilePaths[0], 'test/test.jpeg', function(res){
+          console.log(`上传进度：${res.progress}%，已上传${res.totalBytesSent}B，共${res.totalBytesExpectedToSend}B`)
+          // if(res.progress > 50){ // 测试文件上传一半就终止上传
+          //   return false
+          // }
+        })
+        console.log(result)
       }
     })
   },
-  uploadFile: function(filePath) {
-    wx.showLoading({ title: '正在上传...' });
-    wx.cloud.uploadFile({
-      cloudPath: 'user_photos/' + Date.now() + '.jpg', // 云端保存的文件名
-      filePath: filePath,
-      success: (res) => {
-        wx.hideLoading();
-        this.setData({
-          imageUrl: filePath,
-          showImage: true,
-          uploadResult: '上传成功，云文件ID：' + res.fileID,
-        });
-        this.sendToServer(res.fileID);
-      },
-      fail: (err) => {
-        wx.hideLoading();
-        this.setData({ uploadResult: '上传失败：' + err.errMsg });
-      }
-    })
-  },
-  sendToServer: function(fileID) {
-    wx.request({
-      url: 'https://yourserver.com/api/upload/image', // 替换为你的Django后端API地址
-      method: 'POST',
-      header: {
-        'content-type': 'application/json' // 按实际情况调整请求头
-      },
-      data: JSON.stringify({ file_id: fileID }), // 将fileID转换为JSON格式发送给后端
-      success: (res) => {
-        if (res.statusCode === 200) {
-          this.setData({ uploadResult: '图片已成功发送至服务器，处理结果：' + res.data.message });
-        } else {
-          this.setData({ uploadResult: '发送图片至服务器失败：' + res.data.error });
+  /**
+   * 上传文件到微信云托管对象存储
+   * @param {*} file 微信本地文件，通过选择图片，聊天文件等接口获取
+   * @param {*} path 对象存储路径，根路径直接填文件名，文件夹例子 test/文件名，不要 / 开头
+   * @param {*} onCall 上传回调，文件上传过程监听，返回false时会中断上传
+   */
+  uploadFile(file, path, onCall = () => {}) {
+    return new Promise((resolve, reject) => {
+      const task = wx.cloud.uploadFile({
+        cloudPath: path,
+        filePath: file,
+        config: {
+          env: 'prod-0gi5h53v7bb1a7c8' // 需要替换成自己的微信云托管环境ID
+        },
+        success: res => resolve(res.fileID),
+        fail: e => {
+          const info = e.toString()
+          if (info.indexOf('abort') != -1) {
+            reject(new Error('【文件上传失败】中断上传'))
+          } else {
+            reject(new Error('【文件上传失败】网络或其他错误'))
+          }
         }
-      },
-      fail: (err) => {
-        this.setData({ uploadResult: '发送图片至服务器失败：' + err.errMsg });
-      }
-    });
+      })
+      task.onProgressUpdate((res) => {
+        if (onCall(res) == false) {
+          task.abort()
+        }
+      })
+    })
   }
 })
