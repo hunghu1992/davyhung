3ed459f5a0c640e60a39779847ee9f24a65e23db
davyhung <534900793@qq.com>
2024-02-06 16:36:11 +0800

    test2

--- pages/uploadpage/uploadpage.js
+++ pages/uploadpage/uploadpage.js
@@ -1,52 +1,97 @@
+const app = getApp();
+
+// 定义环境变量
+const ENV_ID = 'prod-0gi5h53v7bb1a7c8'; // 替换成实际环境ID
+
+// 页面配置
 Page({
-  async onLoad() {
-    wx.cloud.init({
-      env: 'prod-0gi5h53v7bb1a7c8',
-    })
-    const that = this
-    await wx.chooseImage({
-      count: 1,
-      async success(res){
-        console.log(res.tempFilePaths)
-        const result = await that.uploadFile(res.tempFilePaths[0], 'test/test.jpeg', function(res){
-          console.log(`上传进度：${res.progress}%，已上传${res.totalBytesSent}B，共${res.totalBytesExpectedToSend}B`)
-          // if(res.progress > 50){ // 测试文件上传一半就终止上传
-          //   return false
-          // }
-        })
-        console.log(result)
-      }
-    })
+  data: {
+    showUploadStatus: false,
+    uploadProgress: 0,
+    uploadResultMessage: '',
+    uploadResultType: '',
   },
-  /**
-   * 上传文件到微信云托管对象存储
-   * @param {*} file 微信本地文件，通过选择图片，聊天文件等接口获取
-   * @param {*} path 对象存储路径，根路径直接填文件名，文件夹例子 test/文件名，不要 / 开头
-   * @param {*} onCall 上传回调，文件上传过程监听，返回false时会中断上传
-   */
-  uploadFile(file, path, onCall = () => {}) {
+
+  onLoad() {
+    wx.cloud.init({ env: ENV_ID });
+  },
+
+  startUploadProcess() {
+    this.setData({ showUploadStatus: true });
+  },
+
+  clearUploadStatus() {
+    this.setData({
+      uploadResultMessage: '',
+      uploadResultType: '',
+      uploadProgress: 0,
+      showUploadStatus: false,
+    });
+  },
+
+  showUploadError(errorMessage) {
+    this.setData({
+      uploadResultMessage: errorMessage,
+      uploadResultType: 'error',
+    });
+  },
+
+  showUploadSuccess(successMessage) {
+    this.setData({
+      uploadResultMessage: successMessage,
+      uploadResultType: 'success',
+    });
+  },
+
+  // 新增：生成唯一文件路径的方法
+  generateUniqueFilePath(tempFilePath) {
+    const date = new Date();
+    const formattedDate = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}_${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    const extension = tempFilePath.substring(tempFilePath.lastIndexOf('.') + 1);
+    const uniquePath = `uploads/${formattedDate}_1.${extension}`;
+    return uniquePath;
+  },
+
+  async onUploadClick() {
+    const _this = this;
+    _this.startUploadProcess();
+    try {
+      // 使用wx.chooseImage让用户选择图片
+      const { tempFiles } = await wx.chooseImage({
+        count: 1, // 只允许用户选择一张图片
+        sizeType: ['original', 'compressed'],
+        sourceType: ['album', 'camera'],
+      });
+
+      if (tempFiles.length > 0) {
+        const tempFilePath = tempFiles[0].path; // 获取第一张选中的图片临时路径
+        const dynamicPath = await this.generateUniqueFilePath(tempFilePath);
+        const fileID = await this.uploadFileToCloudStorage(tempFilePath, dynamicPath);
+        this.showUploadSuccess('文件上传成功');
+      }
+    } catch (error) {
+      _this.showUploadError(error.message || '文件上传失败，请重试');
+    } finally {
+      setTimeout(() => _this.clearUploadStatus(), 2000);
+    }
+  },
+
+  // ... 其他相关方法 ...
+
+  updateUploadProgress(progress) {
+    this.setData({ uploadProgress: progress });
+  },
+
+  async uploadFileToCloudStorage(file, cloudPath) {
     return new Promise((resolve, reject) => {
-      const task = wx.cloud.uploadFile({
-        cloudPath: path,
+      wx.cloud.uploadFile({
+        cloudPath,
         filePath: file,
-        config: {
-          env: 'prod-0gi5h53v7bb1a7c8' // 需要替换成自己的微信云托管环境ID
-        },
-        success: res => resolve(res.fileID),
-        fail: e => {
-          const info = e.toString()
-          if (info.indexOf('abort') != -1) {
-            reject(new Error('【文件上传失败】中断上传'))
-          } else {
-            reject(new Error('【文件上传失败】网络或其他错误'))
-          }
-        }
-      })
-      task.onProgressUpdate((res) => {
-        if (onCall(res) == false) {
-          task.abort()
-        }
-      })
-    })
+        config: { env: "prod-0gi5h53v7bb1a7c8" },
+        success: (res) => resolve(res.fileID),
+        fail: (e) => reject(new Error(`【文件上传失败】${e.errMsg}`)),
+        onProgressUpdate: (res) => this.updateUploadProgress(res.progress),
+      });
+    });
   }
-})
+});
